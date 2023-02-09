fa2d18be361bf415c4547f0deac1261d129e9fc7
ahmed-sallam <devahmedsallam@gmail.com>
2023-02-09 12:12:01 +0300

    first commit

---
+++ src/main/java/com/techpeak/your_openion/YourOpenionApplication.java
@@ -0,0 +1,13 @@
+package com.techpeak.your_openion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourOpenionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourOpenionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techpeak/your_openion/YourOpenionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techpeak.your_openion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourOpenionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
